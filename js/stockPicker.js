a65438b857cce4a0de2cc6e068a73ad9904e5244
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-06-06 16:11:01 -0400

    first

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,19 @@
 exports.picker = function(prices) {
+    let max_profit = prices[1] - prices[0]
+    let min_index = 0
+    let buy_index = 0
+    let sell_index = 1
+
+    for (let i = 1; i < prices.length; i++){
+        if (prices[i] - prices[min_index] > max_profit) {
+            max_profit = prices[i] - prices[min_index]
+            sell_index = i
+            buy_index = min_index
+        }
+        if (prices[i] < prices[min_index]) {
+            min_index = i
+        }
+    }
+    return [buy_index, sell_index]
 
 }
