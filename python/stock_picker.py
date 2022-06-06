f42bba4a0edcaab841a33b36c5a51243e9db1562
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-06-06 15:58:25 -0400

    first

--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,15 @@
 def picker(prices):
-    pass
+    max_profit = prices[1] - prices[0]
+    min_index = 0
+    buy_index = 0
+    sell_index = 1
+
+    for i in range(1, len(prices)):
+        if prices[i] - prices[min_index] > max_profit:
+            max_profit = prices[i] - prices[min_index]
+            sell_index = i
+            buy_index = min_index
+        if prices[i] < prices[min_index]:
+            min_index = i
+    
+    return[buy_index, sell_index]
